32d158883ff360d82e834cf4d0d155ec8a96906f
Rutik Patel <rutikpatel3660@gmail.com>
2022-04-25 15:56:22 -0400

    Updates

---
+++ precache-manifest.f4c1a421822432918dffac0106f5ca82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1a24066b41d63048fcd4df26ec798d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fab71d340ef739047596",
+    "url": "/portfolio/static/css/main.c34096a5.chunk.css"
+  },
+  {
+    "revision": "91b330376acf5848e5cc",
+    "url": "/portfolio/static/js/2.00f94f91.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/portfolio/static/js/2.00f94f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab71d340ef739047596",
+    "url": "/portfolio/static/js/main.d37f5508.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f4248d666c26f00b15911f1f754efebd",
+    "url": "/portfolio/static/media/MyProfile.f4248d66.png"
+  },
+  {
+    "revision": "c46419d94585a7a26f4a1c01a1ef526e",
+    "url": "/portfolio/static/media/gbc.c46419d9.png"
+  },
+  {
+    "revision": "e737ef57a21d4b9362f5c06d9e4f74d8",
+    "url": "/portfolio/static/media/logo-blue.e737ef57.png"
+  },
+  {
+    "revision": "75cb8dae57f2e310c55bbe2c4bb6b6b3",
+    "url": "/portfolio/static/media/walmart.75cb8dae.jpeg"
+  }
+]);
